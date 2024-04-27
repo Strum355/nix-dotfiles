a4617d2c3df02266a6708efb851707f5a315b69a
Noah Santschi-Cooney <noah@santschi-cooney.ch>
2024-04-27 18:17:05 +0100

    bump starpls to 0.1.12

--- pkgs/starpls.nix
+++ pkgs/starpls.nix
@@ -3,16 +3,15 @@
 , fetchFromGitHub
 , protobuf
 }:
-
 rustPlatform.buildRustPackage rec {
   pname = "starpls";
-  version = "0.1.11-unstable-2024-04-17";
+  version = "0.1.12";
 
   src = fetchFromGitHub {
     owner = "withered-magic";
     repo = "starpls";
-    rev = "7eac4b62748dbae714e374aca9922c2fc6dc6cc0";
-    sha256 = "sha256-XJGJ9RPA6mZP/CqbnrjHdONB+otHyRKeyA8CbJt2l1M=";
+    rev = "v${version}";
+    sha256 = "sha256-cPJdESD290KTbhzrjLjYuJmdWI/IUnYZP2pyFJs21Go=";
   };
 
   RUSTC_BOOTSTRAP = 1;
